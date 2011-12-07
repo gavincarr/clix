6e385bd9bc5e56d6c620b422ad0472fdde83abdb
Gavin Carr <gavin@openfusion.com.au>
2011-12-07 20:13:03 +1100

    Refactor Clix::Colour, splitting out separate Clix::Utils.

--- lib/Clix/Colours.pm
+++ lib/Clix/Colours.pm
@@ -6,15 +6,14 @@
 package Clix::Colours;
 
 use strict;
-
-use Exporter;
+use Exporter::Lite;
 use Term::ANSIColor;
-use Time::Piece;
-use Carp;
-use Regexp::Common qw(URI microsyntax);
 
 our @EXPORT = ();
-our @EXPORT_OK = qw(merge_colours colourise);
+our @EXPORT_OK = qw(
+  merge_colours
+  colour_print
+);
 
 my %COLOUR = (
   timestamp     => 'white',
@@ -35,97 +34,13 @@ sub merge_colours {
   @COLOUR{ keys %$user_colours } = values %$user_colours;
 }
 
-sub _colourise_snippet {
-  my ($snippet, $colour, %opts) = @_;
-  my $report_colours = delete $opts{report_colours};
-  croak "Invalid options to _colourise_snippet: " . join(',',keys %opts) if keys %opts;
+# Colourise and print the given set of hashref (text,type) elements
+sub colour_print {
+  my @elts = @_;
 
-  if ($report_colours) {         # Testing
-    return "$colour: $snippet\n";
-  }
-  else {
-    return color($colour) . $snippet;
-  }
-}
-
-sub _colourise_msg {
-  my ($msg, %opts) = @_;
-  my $msg_colour = delete $opts{msg} || 'im_msg';
-  my $report_colours = delete $opts{report_colours};
-  croak "Invalid msg colour: $msg_colour" unless exists $COLOUR{$msg_colour};
-  croak "Invalid options to _colourise_msg: " . join(',',keys %opts) if keys %opts;
-
-  # Tokenise $msg into snippets
-  my $out = '';
-  TOKEN: {
-    $out .= _colourise_snippet($1, $COLOUR{person}, report_colours => $report_colours),
-      redo TOKEN
-        if $msg =~ m/\G( $RE{microsyntax}{user} \s* )/gcx;
-    $out .= _colourise_snippet($1, $COLOUR{hashtag}, report_colours => $report_colours),
-      redo TOKEN
-        if $msg =~ m/\G( $RE{microsyntax}{hashtag} \s* )/gcx;
-    $out .= _colourise_snippet($1, $COLOUR{group}, report_colours => $report_colours),
-      redo TOKEN
-        if $msg =~ m/\G( $RE{microsyntax}{grouptag} \s* )/gcx;
-    $out .= _colourise_snippet($1, $COLOUR{uri}, report_colours => $report_colours),
-      redo TOKEN
-        if $msg =~ m/\G( $RE{URI} \s* )/gcx;
-    $out .= _colourise_snippet($1, $COLOUR{$msg_colour}, report_colours => $report_colours),
-      redo TOKEN
-        if $msg =~ m/\G( (?: \S|\w+ ) \s* )/gcx;
-  }
- 
-  return $out;
-}
-
-# Colourise and print a given message
-sub colourise {
-  my ($msg, %opts) = @_;
-  my $jid = delete $opts{jid};
-  my $mapped_jid = delete $opts{mapped_jid};
-  my $mb_username = delete $opts{mb_username};
-  croak "Invalid options to colourise: " . join(',',keys %opts) if keys %opts;
-
-  print color $COLOUR{timestamp};
-  printf "%s ", localtime->strftime('%T');
-
-  if ($mapped_jid) {
-    print color $COLOUR{from_jid};
-    printf "[%s] ", $mapped_jid;
-  }
-
-  # If this is a microblog account, we jump through special hoops
-  if ($mb_username) {
-    if ($msg =~ m/^\s*(\S+)\s+(.*?)$/s) {
-      my $first_chunk = $1;
-      my $rest = $2;
-      my $username = '';
-
-      if ($first_chunk =~ m/^([-\w]+):$/ ||
-          $first_chunk =~ m/^<([-\w]+)>$/) {
-        $username = $1;
-        $msg = $rest;
-        print color $COLOUR{mb_sender};
-        print "\@$username ";
-      }
-
-      if ($username eq $mb_username) {
-        print _colourise_msg( $msg, msg => 'mb_mine');
-      }
-      elsif ($msg =~ m/\@$mb_username\b/) {
-        print _colourise_msg( $msg, msg => 'mb_reply' );
-      }
-      else {
-        print _colourise_msg( $msg, msg => 'mb_msg' );
-      }
-    }
-    else {
-      print _colourise_msg( $msg, msg => 'mb_msg' );
-    }
-  }
-  
-  else {
-    print _colourise_msg( $msg, msg => 'im_msg' );
+  for my $elt (@elts) {
+    print color($COLOUR{ $elt->{type} } || 'white');
+    print $elt->{text};
   }
 
   print color 'reset';
---
+++ lib/Clix/Utils.pm
@@ -0,0 +1,117 @@
+#!/usr/bin/perl
+#
+# clix utility routines
+#
+
+package Clix::Utils;
+
+use strict;
+use Exporter::Lite;
+use Regexp::Common qw(URI microsyntax);
+use Time::Piece;
+use Carp;
+
+use Clix::Colours qw(colour_print);
+
+our @EXPORT = ();
+our @EXPORT_OK = qw(
+  tokenise_msg
+  prep_msg
+  print_msg
+);
+
+# Return an array of tokens (hashrefs) for $msg
+sub tokenise_msg {
+  my ($msg, $msg_type) = @_;
+  my @token;
+
+  TOKEN: {
+    push(@token, { text => $1, type => 'person' }),     redo TOKEN
+      if $msg =~ m/\G( $RE{microsyntax}{user} \s* )/gcx;
+    push(@token, { text => $1, type => 'hashtag' }),    redo TOKEN
+      if $msg =~ m/\G( $RE{microsyntax}{hashtag} \s* )/gcx;
+    push(@token, { text => $1, type => 'group' }),      redo TOKEN
+      if $msg =~ m/\G( $RE{microsyntax}{grouptag} \s* )/gcx;
+    push(@token, { text => $1, type => 'uri' }),        redo TOKEN
+      if $msg =~ m/\G( $RE{URI} \s* )/gcx;
+    push(@token, { text => $1, type => $msg_type }),    redo TOKEN
+      if $msg =~ m/\G( (?: \w+|\S ) \s* )/gcx;
+  }
+
+  # Coalese tokens of the same type
+  my @coalesced;
+  for (@token) {
+    # Push first
+    if (! @coalesced) {
+      push @coalesced, $_;
+    }
+    # Append if same type as last, otherwise push
+    else {
+      if ($coalesced[$#coalesced]->{type} eq $_->{type}) {
+        $coalesced[$#coalesced]->{text} .= $_->{text};
+      }
+      else {
+        push @coalesced, $_;
+      }
+    }
+  }
+ 
+  return @coalesced;
+}
+
+# Return an array of elements (hashrefs) representing $msg
+sub prep_msg {
+  my ($msg, %opts) = @_;
+  my $mapped_jid  = $opts{mapped_jid};
+  my $mb_username = $opts{mb_username};
+  my $msg_type;
+
+  my @output;
+  push @output, { text => localtime->strftime('%T '), type => 'timestamp' };
+  push @output, { text => "[$mapped_jid] ", type => 'from_jid' } if $mapped_jid;
+
+  # Parse out leading username on microblog messages
+  if ($mb_username) {
+    my $username = '';
+    if ($msg =~ m/^\s*(\S+)\s+(.*?)$/s) {
+      my $first_chunk = $1;
+      my $rest = $2;
+      $username = '';
+
+      if ($first_chunk =~ m/^ ([-\w]+):$/x ||
+          $first_chunk =~ m/^<([-\w]+)>$/x) {
+        $username = $1;
+        $msg = $rest;
+        push @output, { text => "\@$username ", type => 'mb_sender' };
+      }
+    }
+
+    # Set mb msg_type
+    if ($username && $username eq $mb_username) {
+      $msg_type = 'mb_mine';
+    }
+    elsif ($msg =~ m/\@$mb_username\b/) {
+      $msg_type = 'mb_reply';
+    }
+    else {
+      $msg_type = 'mb_msg';
+    }
+  }
+  else {
+    $msg_type = 'im_msg';
+  }
+
+  push @output, tokenise_msg($msg, $msg_type);
+
+  return @output;
+}
+
+# Prep and output colourised $msg
+sub print_msg {
+  my ($msg, %opts) = @_;
+  my @output = prep_msg($msg, %opts);
+  colour_print(@output);
+}
+
+1;
+
